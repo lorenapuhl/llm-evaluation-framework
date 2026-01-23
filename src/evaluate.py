eb04b382698d98397d809c268c546d494675ca19
Lorena Puhl <lorena.puhl@protonmail.com>
2026-01-23 13:48:22 -0600

    fix: add root-directory to import directories in src/evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,6 +1,12 @@
 """
 Enhanced Text Evaluation Framework with Semantic Understanding and Category-Aware Metrics
 """
+#set import directories
+import sys
+import os
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_root)
 
 import re
 import numpy as np
